b6e5701243e44f0374ed6567bc1f808a4f4764b3
Sajid <empezar.sajid@gmail.com>
2018-10-25 18:16:57 +0530

    Status added

---
+++ common/Functions/Status.cs
@@ -0,0 +1,13 @@
+namespace Common
+{
+
+    public static class Status
+    {
+        public const string PENDING = "PENDING";
+        public const string INPROGRESS = "IN-PROGRESS";
+        public const string ONHOLD = "ON-HOLD";
+
+        public const string COMPLETED = "COMPLETED";
+    }
+
+}
